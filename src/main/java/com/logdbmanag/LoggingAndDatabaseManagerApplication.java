941d7a863ec4b2f47daba39cd32e3fc40202375c
Warded120 <greenvania11@gmail.com>
2025-02-26 14:59:59 +0200

    initial commit

---
+++ src/main/java/com/logdbmanag/LoggingAndDatabaseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.logdbmanag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingAndDatabaseManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingAndDatabaseManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logdbmanag/LoggingAndDatabaseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logdbmanag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingAndDatabaseManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
